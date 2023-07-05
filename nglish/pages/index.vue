f59138b7dd4fdbcea942800bf13ff882c3bf3131
tahir-kali <irenic.tahir@gmail.com>
2023-07-05 15:15:37 +0500

    Product: Nglish with Rahimi

---
+++ nglish/pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+ <div>
+  <div class="header text-white px-10 py-5 text-center bg-green-500">
+    Welcome to Learning <span class="logo">.nGlish</span> with Rahimi
+ </div>
+ </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'IndexPage'
+})
+</script>
+<style>
+.header{
+  font-size: 30px
+}
+.logo{
+  padding:10px;
+  border: 1px solid black;
+  border-radius: 15px;
+  background-color: black !important;
+}
+</style>
